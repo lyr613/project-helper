b492c74431f44f4a933f29e7e59452e549d8bee5
qsoft <qiusoft613@gmail.com>
2020-01-03 16:01:38 +0800

    perf: 现在默认优先展示预览图多的项目

--- packages/page/src/arena/stage/shelf/subj.ts
+++ packages/page/src/arena/stage/shelf/subj.ts
@@ -11,12 +11,16 @@ export const list_filtered$ = app_list$.pipe(
 	switchMap(li => {
 		return filter$.pipe(
 			map(opt => {
-				return li.filter(app => {
-					if (opt.type !== 'all') {
-						return app.type === opt.type
-					}
-					return true
-				})
+				return li
+					.filter(app => {
+						if (opt.type !== 'all') {
+							return app.type === opt.type
+						}
+						return true
+					})
+					.sort((a, b) => {
+						return b.previews.length - a.previews.length
+					})
 			}),
 		)
 	}),
