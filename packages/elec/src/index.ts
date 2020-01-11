dcca0c0e35b10748a5a512dbdea17146dff26563
qsoft <qiusoft613@gmail.com>
2020-01-11 12:13:14 +0800

    feat: 自动下载修改提示方式

--- packages/elec/src/index.ts
+++ packages/elec/src/index.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, dialog } from 'electron'
 import { create_option, did_create } from '@/create'
 import { watch } from './watch'
 import { autoUpdater } from 'electron-updater'
@@ -12,7 +12,14 @@ process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
 let main_window: any
 
 app.once('ready', () => {
-    autoUpdater.autoDownload = true
+    autoUpdater.checkForUpdates()
+    autoUpdater.on('update-available', () => {
+        dialog.showMessageBox({
+            title: '有更新',
+            message: '有更新了, 自动下载',
+        })
+        autoUpdater.downloadUpdate()
+    })
     createWindow()
 })
 
