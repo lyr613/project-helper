3b347a53250f98c22641f813eddae92fdd32e4ee
qsoft <qiusoft613@gmail.com>
2020-01-19 14:41:19 +0800

    chore: 隐藏token

--- packages/elec/scripts/publish.js
+++
@@ -1,8 +0,0 @@
-const cp = require('child_process')
-
-const d = cp.exec(` npx cross-env GH_TOKEN=74b7d6fd025e7aaf84977528d987893d51e01bde npx electron-builder `)
-
-const out = d.stdout
-out.on('data', (e) => {
-    console.log(e)
-})
