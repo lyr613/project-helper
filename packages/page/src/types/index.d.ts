01e74af9f6667e94c396b4ffefc3e03fa63b69d2
qsoft <qiusoft613@gmail.com>
2020-01-21 07:51:49 +0800

    del: 无用类型声明

--- packages/page/src/types/index.d.ts
+++ packages/page/src/types/index.d.ts
@@ -1,16 +1,10 @@
-import './npc'
 import 'electron'
-import './book'
 
 declare global {
-    interface Param {
-        [k: string]: any
-    }
-    interface Window {
-        electron: Electron.CommonInterface
-    }
-    type book = Book.book
-    type chapter = Book.chapter
-    type node = Book.node
-    type npc = Npc.npc
+	interface Param {
+		[k: string]: any
+	}
+	interface Window {
+		electron: Electron.CommonInterface
+	}
 }
